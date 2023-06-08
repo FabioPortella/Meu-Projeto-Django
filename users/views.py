be338b421fcd1ede2b5ac0a0e00f2ddd444c5336
FabioPortella <fabioportella2009@gmail.com>
2023-06-08 16:07:22 -0300

    alteração no register - acrescentar email

--- users/views.py
+++ users/views.py
@@ -4,6 +4,19 @@ from django.urls import reverse
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
+from django import forms
+from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    #first_name = forms.CharField(label='Nome', max_length=30)
+    #last_name = forms.CharField(label='Sobrenome', max_length=30)
+    email = forms.EmailField(label='Email', required=True)
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        #fields = UserCreationForm.Meta.fields + ('first_name', 'last_name', 'email',)
+        fields = UserCreationForm.Meta.fields + ('email',)
+
 
 def logout_view(request):
     """Faz o Log out do usuário"""
@@ -15,16 +28,16 @@ def register(request):
     """Faz o cadastro de um novo usuário"""
     if request.method != 'POST':
         # Exibe o formulário de cadastro em branco
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     else:
         # Processa o formulário preenchido
-        form = UserCreationForm(data=request.POST)
+        form = CustomUserCreationForm(data=request.POST)
 
         if form.is_valid():
-            new_user = form.save()
+            novo_usuario = form.save()
             # Faz o login do usuário e o redireciona para a página inicial
-            authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
-            login(request, authenticated_user)
+            autentificacao = authenticate(username=novo_usuario.username, password=request.POST['password1'])
+            login(request, autentificacao)
             return HttpResponseRedirect(reverse('noticias:index'))
         
     template = 'users/register.html'
