0ce1c6ecdc2240faa7274c86680f892141838424
FabioPortella <fabioportella2009@gmail.com>
2023-06-08 13:52:40 -0300

    criando app users para login, register e logout

--- proj_blog_noticias/urls.py
+++ proj_blog_noticias/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("app_noticias.urls", namespace="noticias")),
+    path('users/', include("users.urls", namespace="users")), 
 ]
